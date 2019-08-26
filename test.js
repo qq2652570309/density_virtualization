d6ea24026f45f207372e6c5cd4b93de7d62a11fb
wentian ubuntu <wbai1295@gmail.com>
2019-08-26 08:51:44 -0400

    start coordinate bug not fix

--- test.js
+++ test.js
@@ -31,18 +31,38 @@ function getRandomColor() {
     return color;
 }
 
-var create_cell = function(coordinates, axis) {
-    let new_cell = []
+// create a new cell in grid
+let new_cell = function(size, coordinates, axis) {
+    cell = []
+
+    // create a cell by one coordinate which is left-bottom
+    // its size is based on argument: size
+    // in this case, axis is useless
+    if (coordinates.length === 2) {
+        cell.push([coordinates[0], coordinates[1]])
+        cell.push([coordinates[0], coordinates[1] + size])
+        cell.push([coordinates[0] + size, coordinates[1] + size])
+        cell.push([coordinates[0] + size, coordinates[1]])
+        cell.push([coordinates[0], coordinates[1]])
+
+        return cell
+    }
+
+    // create a cell by another cell
     if (axis === 0) {
         for (let j=0; j < coordinates.length; j++) {
-            new_cell.push([coordinates[j][0]+1, coordinates[j][1]])
+            cell.push([coordinates[j][0] + size, coordinates[j][1]])
         }
     } else if (axis === 1) {
         for (let j=0; j < coordinates.length; j++) {
-            new_cell.push([coordinates[j][0], coordinates[j][1]+1])
+            cell.push([coordinates[j][0], coordinates[j][1] + size])
         }
     }
-    // console.log(coordinates)
+    return cell
+}
+
+let new_feature = function(coordinates, axis) {
+    let cell = new_cell(1, coordinates, axis)
     return {
         "type": "Feature",
         "properties": {
@@ -55,22 +75,24 @@ var create_cell = function(coordinates, axis) {
         "geometry": {
             "type": "Polygon",
             "coordinates": [
-                new_cell
+                cell
             ]
         }
     }
 }
 
+// geo.features[0].geometry.coordinates[0] = new_cell(1,[123.61, -22.14])
+geo.features[0].geometry.coordinates[0] = new_cell(1,[43.04, -76.14])
 
 for (let i=1; i < 32; i++) {
     let cell = geo.features[i-1].geometry.coordinates[0]
-    let new_cell = create_cell(cell, axis=0)
-    geo.features.push(new_cell)
+    let feature = new_feature(cell, axis=0)
+    geo.features.push(feature)
 }
 
 // for (let i=0; i < 32; i++) {
 //     let cell = geo.features[i].geometry.coordinates[0]
-//     let new_cell = create_cell(cell, axis=1)
+//     let new_cell = new_feature(cell, axis=1)
 //     geo.features.push(new_cell)
 // }
 
@@ -79,9 +101,14 @@ for (let j=0; j < 32; j++) {
     let k = (j+1)*32
     for (; i < k; i++) {
         let cell = geo.features[i].geometry.coordinates[0]
-        let new_cell = create_cell(cell, axis=1)
-        geo.features.push(new_cell)
+        let feature = new_feature(cell, axis=1)
+        geo.features.push(feature)
     }
 }
 
-  
+
+// console.log(new_cell(1,[123.61, -22.14]))
+// console.log(new_cell(0.1,[123.61, -22.14]))
+
+// console.log(geo.features[0].geometry.coordinates[0])
+// console.log(new_cell(1,[123.61, -22.14]))
